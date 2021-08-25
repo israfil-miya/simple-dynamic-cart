b6bd668d11b10e475248d47406755f0340959c75
Israfil Miya <israfilmiya120@gmail.com>
2021-08-25 13:08:26 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// price database
 let pricing = {
   memory: {
     memory_8: 250,
@@ -14,9 +15,14 @@ let pricing = {
   }
 }
 
+// selcection of options wrapper div
 let selection_area = document.getElementById("selection_area")
+
+// discount
 let discount = 150;
 let discount_codeName = "WOW69";
+
+// final total price calculating
 function calculate_price(total) {
   if (document.getElementById("final_total").getAttribute("class") == "discounted") {
     return total-discount;
@@ -24,33 +30,52 @@ function calculate_price(total) {
     return total;
   }
 }
+
+// pre-defining default values ( only item price )
+document.getElementById("total_price").innerHTML = document.getElementById("fixed_price").innerHTML;
+document.getElementById("final_total").innerHTML = calculate_price(document.getElementById("fixed_price").innerHTML);
+
+
+// on option click event handling
 selection_area.addEventListener('click', function(e) {
-  let target_option = e.target;
-  let item_cat = target_option.classList[0];
-  let item_option_value = target_option.getAttribute("id")
-  let item_price = pricing[item_cat][item_option_value];
+  let target_option = e.target; // targeting percular option
+  let item_cat = target_option.classList[0]; // getting option category
+  let item_option_value = target_option.getAttribute("id") // getting the option
+  let item_price = pricing[item_cat][item_option_value]; // pulling out the price from database
+
+  // showing which option slected in client-side
   let total_price_option = document.getElementById(item_cat+"_total");
   total_price_option.innerHTML = item_price;
+  
+  // calculating the chosen options total price
   let total = 0;
   let total_ele = document.querySelectorAll('.total');
-
   for (let x = 0; x < total_ele.length; x++) {
     let price = parseInt(total_ele[x].innerHTML);
     total += price;
   }
-
+  
+  // defining the final output/total price ( dynamic )
   document.getElementById("final_total").innerHTML = calculate_price(total);
-
   document.getElementById("total_price").innerHTML = total;
+  
 });
+
+// promo-code functionality
 function promoCode() {
+  
+  // selecting promo-code input and submit button element
   let inputEle = document.getElementById("promo_code");
   let inputBtn = document.getElementById("promo_btn");
+  
+  // main promo-code logic
   if (inputEle.value == discount_codeName) {
     let total_price = parseInt(document.getElementById("final_total").innerHTML)
     if (total_price != 0 && total_price >= discount) {
       document.getElementById("final_total").innerHTML = total_price-discount;
       document.getElementById("final_total").setAttribute("class", "discounted");
+      
+      // making of " can't use promo-code again once used " functionality
       inputEle.value = "";
       inputBtn.setAttribute("disabled", true)
       inputEle.setAttribute("disabled", true)
